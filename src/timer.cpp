64676dca22ae9e690c3ef74688ded3c0edd45aa0
Charly Mourglia <charly.mourglia@diginext.fr>
2020-02-02 22:26:05 +0100

    Added a timer class

---
+++ src/timer.cpp
@@ -0,0 +1,41 @@
+#include <eastl/chrono.h>
+
+using clock     = eastl::chrono::high_resolution_clock;
+using timepoint = eastl::chrono::high_resolution_clock::time_point;
+
+class timer
+{
+public:
+	inline void Start()
+	{
+		m_Start    = clock::now();
+		m_LastTime = m_Start;
+	}
+
+	inline void Restart()
+	{
+		Start();
+	}
+
+	inline double DeltaTime() const
+	{
+		const timepoint                 Time = clock::now();
+		eastl::chrono::duration<double> dt   = Time - m_LastTime;
+
+		m_LastTime = Time;
+
+		return dt.count();
+	}
+
+	inline double GetElapsedSeconds() const
+	{
+		const timepoint                 Time = clock::now();
+		eastl::chrono::duration<double> dt   = Time - m_Start;
+
+		return dt.count();
+	}
+
+private:
+	timepoint         m_Start;
+	mutable timepoint m_LastTime;
+};
