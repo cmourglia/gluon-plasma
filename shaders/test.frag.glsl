b5ec9186035960c707414a6a4742b2c6280c45f9
Charly Mourglia <charly.mourglia@diginext.fr>
2020-03-20 20:07:54 +0100

    Shader reload WIP: Rounded rectangles

--- shaders/test.frag.glsl
+++ shaders/test.frag.glsl
@@ -1,6 +1,7 @@
 #version 450
 
 layout (location = 0) flat in uint InstanceID;
+layout (location = 1) in vec2 InPosition;
 
 layout (location = 0) out vec4 out_Color;
 
@@ -16,9 +17,46 @@ layout (std430, binding=1) buffer rect_infos
     rectangle_info[] u_RectangleInfos;
 };
 
-uniform vec4 u_Color;
+uniform vec2 u_ViewportSize;
+uniform mat4 u_View;
+
+float GetRectangleAlpha(vec2 Position, vec2 Center, vec2 Size, float Radius)
+{
+    vec2 Q = abs(Position - Center) - Size + Radius;
+    return (min(max(Q.x, Q.y), 0.0) + length(max(Q, 0.0)) - Radius);
+
+    float Distance = length(Position - Center);
+
+    return 1 - step(Radius, Distance);
+}
 
 void main()
 {
-    out_Color = u_RectangleInfos[InstanceID].FillColor;
+    // vec2 Position = (2.0 * gl_FragCoord.xy / u_ViewportSize) / u_ViewportSize.y;
+    vec2 Position = InPosition;
+    vec2 Center = vec2(u_View * vec4(u_RectangleInfos[InstanceID].PositionSize.xy, 0, 1));
+    // vec2 Center = (2.0 * u_RectangleInfos[InstanceID].PositionSize.xy / u_ViewportSize) / u_ViewportSize.y;
+    // Center.y = 1 - Center.y;
+
+    // vec2 Size = (2.0 * u_RectangleInfos[InstanceID].PositionSize.zw / u_ViewportSize) / u_ViewportSize.y;
+    vec2 Size = u_RectangleInfos[InstanceID].PositionSize.zw;
+    float Radius = Size.x;
+
+    float Alpha = GetRectangleAlpha(Position, Center, Size, Radius) / length(Size);
+    vec3 Color = vec3(1.0) - sign(Alpha) * vec3(0.1, 0.4, 0.7);
+    Color *= 1.0 - exp(-3.0 * abs(Alpha));
+    Color *= 0.8 + 0.2 * cos(150 * Alpha);
+    Color = mix(Color, vec3(1.0, 0.0, 0.0), 1.0 - smoothstep(0.0, 0.05, abs(Alpha)));
+
+    float Sign = sign(Alpha);
+
+    out_Color = u_RectangleInfos[InstanceID].FillColor * Alpha;
+    out_Color.rgb = Color;
+    // out_Color = vec4(Alpha);
+    // out_Color = vec4(vec3(Alpha), 1);
+    // out_Color.a = Sign;
+    // out_Color = vec4(Position, 0, 1);
+    // out_Color = vec4(Center, 0, 1);
+    // out_Color = vec4(0.5, 0.5, 0, 1);
+    out_Color.a = 1;
 }
--- shaders/test.vert.glsl
+++ shaders/test.vert.glsl
@@ -15,6 +15,7 @@ layout (std430, binding=1) buffer rect_infos
 };
 
 layout (location = 0) flat out uint InstanceID;
+layout (location = 1) out vec2 OutPosition;
 
 uniform mat4 u_View;
 uniform mat4 u_Proj;
@@ -22,9 +23,13 @@ uniform mat4 u_Proj;
 void main()
 {
     vec2 Translation = u_RectangleInfos[gl_InstanceID].PositionSize.xy;
+    Translation = mix(Translation - 15, Translation + 15, in_Position * 0.5 + 0.5);
     vec2 Scale = u_RectangleInfos[gl_InstanceID].PositionSize.zw;
 
     vec2 Position = in_Position * Scale + Translation;
     gl_Position = u_Proj * u_View * vec4(Position, 0, 1);
+
+    OutPosition = vec2(u_View * vec4(Position, 0, 1));
+
     InstanceID = gl_InstanceID;
 }
