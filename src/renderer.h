ad974ab2047ca72f16aa892ec03c6235a093df01
Charly Mourglia <charly.mourglia@diginext.fr>
2020-06-21 00:32:22 +0200

    namespace gln for renderer and backend

--- src/renderer.h
+++ src/renderer.h
@@ -1,20 +1,12 @@
 #pragma once
 
+#include "gln_color.h"
+
+namespace gln {
 struct rendering_context;
 
-struct color
-{
-	float r, g, b;
-	float a = 1.0f;
-};
-
-struct vec2
-{
-	float x, y;
-};
-
 rendering_context* CreateRenderingContext();
-void               DestroyRenderingContext(rendering_context* Context);
+void               DestroyRenderingContext(rendering_context* context);
 
 void Resize(rendering_context* Context, float Width, float Height);
 void SetCameraInfo(rendering_context* Context, const float* ViewMatrix, const float* ProjMatrix);
@@ -29,7 +21,9 @@ void DrawRectangle(rendering_context* Context,
                    float              Width,
                    float              Height,
                    color              FillColor,
+                   float              Radius      = 0.0f,
                    float              BorderWidth = 0.0f,
                    color              BorderColor = {0.0f, 0.0f, 0.0f, 1.0f});
 
 void Flush(rendering_context* Context);
+}
