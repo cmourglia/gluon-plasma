74fc7a6a6d953424410fdea4aad324b9a57150a8
Charly Mourglia <charly.mourglia@diginext.fr>
2020-07-15 14:44:00 +0200

    Only expose relevant functions for the drawing API

--- src/gluon/api/gln_renderer.cpp
+++ src/gluon/api/gln_renderer.cpp
@@ -1,4 +1,5 @@
 #include <gluon/api/gln_renderer.h>
+#include <gluon/api/gln_renderer_p.h>
 #include <gluon/api/gln_text.h>
 
 #include <gluon/render_backend/gln_renderbackend.h>
@@ -22,6 +23,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <loguru.hpp>
 
@@ -121,14 +123,7 @@ struct rendering_context
 	void*         TextInfoSSBOPtr = nullptr;
 };
 
-struct draw_command
-{
-	u32 VertexCount;
-	u32 InstanceCount;
-	u32 FirstIndex;
-	u32 BaseVertex;
-	u32 BaseInstance;
-};
+static rendering_context* g_Context = nullptr;
 
 namespace fs = std::filesystem;
 
@@ -137,90 +132,223 @@ static fs::file_time_type g_RectFShaderTime;
 static fs::file_time_type g_TextVShaderTime;
 static fs::file_time_type g_TextFShaderTime;
 
-rendering_context* CreateRenderingContext()
+namespace priv
 {
-	InitializeBackend();
+	void CreateRenderingContext()
+	{
+		if (g_Context != nullptr)
+		{
+			return;
+		}
+
+		InitializeBackend();
 #ifdef _DEBUG
-	EnableDebugging();
+		EnableDebugging();
 #endif
 
-	rendering_context* Context = new rendering_context();
+		g_Context = new rendering_context();
 
-	{
-		g_RectVShaderTime = fs::last_write_time(fs::path("shaders/rect.vert.glsl"));
-		g_RectFShaderTime = fs::last_write_time(fs::path("shaders/rect.frag.glsl"));
+		{
+			g_RectVShaderTime = fs::last_write_time(fs::path("shaders/rect.vert.glsl"));
+			g_RectFShaderTime = fs::last_write_time(fs::path("shaders/rect.frag.glsl"));
 
-		auto VertexShaderHandle   = CreateShaderFromFile("shaders/rect.vert.glsl", ShaderType_Vertex);
-		auto FragmentShaderHandle = CreateShaderFromFile("shaders/rect.frag.glsl", ShaderType_Fragment);
-		auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
+			auto VertexShaderHandle   = CreateShaderFromFile("shaders/rect.vert.glsl", ShaderType_Vertex);
+			auto FragmentShaderHandle = CreateShaderFromFile("shaders/rect.frag.glsl", ShaderType_Fragment);
+			auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
 
-		Context->RectProgram = ProgramHandle;
+			g_Context->RectProgram = ProgramHandle;
 
-		pos_vertex::Init();
-		u32 VertexBufferSize = (u32)k_QuadVertices.size() * sizeof(pos_vertex);
-		u32 IndexBufferSize  = (u32)k_QuadIndices.size() * sizeof(uint16_t);
+			pos_vertex::Init();
+			u32 VertexBufferSize = (u32)k_QuadVertices.size() * sizeof(pos_vertex);
+			u32 IndexBufferSize  = (u32)k_QuadIndices.size() * sizeof(uint16_t);
 
-		Context->RectVertexBuffer = CreateImmutableBuffer(k_QuadVertices.size() * sizeof(pos_vertex), k_QuadVertices.data());
-		Context->RectIndexBuffer  = CreateImmutableBuffer(k_QuadIndices.size() * sizeof(uint16_t), k_QuadIndices.data());
+			g_Context->RectVertexBuffer = CreateImmutableBuffer(k_QuadVertices.size() * sizeof(pos_vertex), k_QuadVertices.data());
+			g_Context->RectIndexBuffer  = CreateImmutableBuffer(k_QuadIndices.size() * sizeof(uint16_t), k_QuadIndices.data());
 
-		Context->RectVertexArray = CreateVertexArray(Context->RectIndexBuffer);
-		AttachVertexBuffer(Context->RectVertexArray, Context->RectVertexBuffer, pos_vertex::s_Layout);
+			g_Context->RectVertexArray = CreateVertexArray(g_Context->RectIndexBuffer);
+			AttachVertexBuffer(g_Context->RectVertexArray, g_Context->RectVertexBuffer, pos_vertex::s_Layout);
 
-		Context->LastRectangleCount   = 128 * 128;
-		Context->RectangleInfoSSBO    = CreateImmutableBuffer(128 * 128 * sizeof(rectangle));
-		Context->RectangleInfoSSBOPtr = MapBuffer(Context->RectangleInfoSSBO);
+			g_Context->LastRectangleCount   = 128 * 128;
+			g_Context->RectangleInfoSSBO    = CreateImmutableBuffer(128 * 128 * sizeof(rectangle));
+			g_Context->RectangleInfoSSBOPtr = MapBuffer(g_Context->RectangleInfoSSBO);
+		}
+
+		{
+			g_TextVShaderTime = fs::last_write_time(fs::path("shaders/text.vert.glsl"));
+			g_TextFShaderTime = fs::last_write_time(fs::path("shaders/text.frag.glsl"));
+
+			auto VertexShaderHandle   = CreateShaderFromFile("shaders/text.vert.glsl", ShaderType_Vertex);
+			auto FragmentShaderHandle = CreateShaderFromFile("shaders/text.frag.glsl", ShaderType_Fragment);
+			auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
+
+			g_Context->TextProgram = ProgramHandle;
+		}
 	}
 
+	void DestroyRenderingContext()
 	{
-		g_TextVShaderTime = fs::last_write_time(fs::path("shaders/text.vert.glsl"));
-		g_TextFShaderTime = fs::last_write_time(fs::path("shaders/text.frag.glsl"));
-
-		auto VertexShaderHandle   = CreateShaderFromFile("shaders/text.vert.glsl", ShaderType_Vertex);
-		auto FragmentShaderHandle = CreateShaderFromFile("shaders/text.frag.glsl", ShaderType_Fragment);
-		auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
-
-		Context->TextProgram = ProgramHandle;
+		if (glIsProgram(g_Context->RectProgram.Idx))
+		{
+			glDeleteProgram(g_Context->RectProgram.Idx);
+		}
+		delete g_Context;
+		g_Context = nullptr;
 	}
 
-	return Context;
-}
-
-void DestroyRenderingContext(rendering_context* Context)
-{
-	if (glIsProgram(Context->RectProgram.Idx))
+	void Resize(f32 Width, f32 Height)
 	{
-		glDeleteProgram(Context->RectProgram.Idx);
+		g_Context->ViewportWidth  = Width;
+		g_Context->ViewportHeight = Height;
+
+		const auto ViewMatrix = glm::mat4(1.0f);
+		const auto ProjMatrix = glm::orthoLH_ZO(0.0f, (f32)Width, (f32)Height, 0.0f, 0.0f, 100.0f);
+
+		memcpy(g_Context->ViewMatrix, glm::value_ptr(ViewMatrix), 16 * sizeof(f32));
+		memcpy(g_Context->ProjMatrix, glm::value_ptr(ProjMatrix), 16 * sizeof(f32));
 	}
-	delete Context;
+
+	void SetTextScale(f32 ScaleX, f32 ScaleY)
+	{
+		g_Context->TextScaleX = ScaleX;
+		g_Context->TextScaleY = ScaleY;
+	}
+
+	void RenderRectangles()
+	{
+		const i32 RectangleCount = (i32)g_Context->Rectangles.size();
+
+		if (RectangleCount == 0)
+		{
+			return;
+		}
+
+		auto VertexTime   = fs::last_write_time(fs::path("shaders/rect.vert.glsl"));
+		auto FragmentTime = fs::last_write_time(fs::path("shaders/rect.frag.glsl"));
+
+		if (VertexTime > g_RectVShaderTime || FragmentTime > g_RectFShaderTime)
+		{
+			g_RectVShaderTime = VertexTime;
+			g_RectFShaderTime = FragmentTime;
+
+			auto VertexShaderHandle   = CreateShaderFromFile("shaders/rect.vert.glsl", ShaderType_Vertex);
+			auto FragmentShaderHandle = CreateShaderFromFile("shaders/rect.frag.glsl", ShaderType_Fragment);
+			auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
+
+			g_Context->RectProgram = ProgramHandle;
+		}
+
+		glUseProgram(g_Context->RectProgram.Idx);
+
+		const i32 ViewLoc         = glGetUniformLocation(g_Context->RectProgram.Idx, "u_View");
+		const i32 ProjLoc         = glGetUniformLocation(g_Context->RectProgram.Idx, "u_Proj");
+		const i32 ViewportSizeLoc = glGetUniformLocation(g_Context->RectProgram.Idx, "u_ViewportSize");
+
+		glUniformMatrix4fv(ViewLoc, 1, GL_FALSE, g_Context->ViewMatrix);
+		glUniformMatrix4fv(ProjLoc, 1, GL_FALSE, g_Context->ProjMatrix);
+		glUniform2f(ViewportSizeLoc, g_Context->ViewportWidth, g_Context->ViewportHeight);
+
+		if (g_Context->LastRectangleCount < RectangleCount)
+		{
+			UnmapBuffer(g_Context->RectangleInfoSSBO);
+			ResizeImmutableBuffer(&g_Context->RectangleInfoSSBO, RectangleCount * sizeof(rectangle));
+
+			g_Context->LastRectangleCount   = RectangleCount;
+			g_Context->RectangleInfoSSBOPtr = MapBuffer(g_Context->RectangleInfoSSBO);
+		}
+
+		memcpy(g_Context->RectangleInfoSSBOPtr, g_Context->Rectangles.data(), RectangleCount * sizeof(rectangle));
+
+		glBindVertexArray(g_Context->RectVertexArray.Idx);
+
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, g_Context->RectangleInfoSSBO.Idx);
+		glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr, RectangleCount);
+
+		g_Context->Rectangles.clear();
+	}
+
+	void RenderTexts()
+	{
+		const i32 GlyphCount = (i32)g_Context->GlyphData.size();
+
+		auto VertexTime   = fs::last_write_time(fs::path("shaders/text.vert.glsl"));
+		auto FragmentTime = fs::last_write_time(fs::path("shaders/text.frag.glsl"));
+
+		if (VertexTime > g_TextVShaderTime || FragmentTime > g_TextFShaderTime)
+		{
+			g_TextVShaderTime = VertexTime;
+			g_TextFShaderTime = FragmentTime;
+
+			auto VertexShaderHandle   = CreateShaderFromFile("shaders/text.vert.glsl", ShaderType_Vertex);
+			auto FragmentShaderHandle = CreateShaderFromFile("shaders/text.frag.glsl", ShaderType_Fragment);
+			auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
+
+			g_Context->TextProgram = ProgramHandle;
+		}
+
+		glUseProgram(g_Context->TextProgram.Idx);
+
+		const i32 ViewLoc         = glGetUniformLocation(g_Context->TextProgram.Idx, "u_View");
+		const i32 ProjLoc         = glGetUniformLocation(g_Context->TextProgram.Idx, "u_Proj");
+		const i32 ViewportSizeLoc = glGetUniformLocation(g_Context->TextProgram.Idx, "u_ViewportSize");
+
+		glUniformMatrix4fv(ViewLoc, 1, GL_FALSE, g_Context->ViewMatrix);
+		glUniformMatrix4fv(ProjLoc, 1, GL_FALSE, g_Context->ProjMatrix);
+		glUniform2f(ViewportSizeLoc, g_Context->ViewportWidth, g_Context->ViewportHeight);
+
+		if (g_Context->LastGlyphCount < GlyphCount)
+		{
+			UnmapBuffer(g_Context->TextInfoSSBO);
+			ResizeImmutableBuffer(&g_Context->TextInfoSSBO, GlyphCount * sizeof(glyph_data));
+
+			g_Context->LastGlyphCount  = GlyphCount;
+			g_Context->TextInfoSSBOPtr = MapBuffer(g_Context->TextInfoSSBO);
+		}
+
+		memcpy(g_Context->TextInfoSSBOPtr, g_Context->GlyphData.data(), GlyphCount * sizeof(glyph_data));
+
+		glBindVertexArray(g_Context->RectVertexArray.Idx);
+
+		u32 CurrentIndex = 0;
+		for (const auto& Font : g_Context->FontTextures)
+		{
+			glBindTextureUnit(CurrentIndex++, Font.Idx);
+		}
+
+		i32 TextureBindings[] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+		const i32 TexturesLoc = glGetUniformLocation(g_Context->TextProgram.Idx, "u_Textures");
+		glUniform1iv(TexturesLoc, 8, TextureBindings);
+
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, g_Context->TextInfoSSBO.Idx);
+		glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr, GlyphCount);
+
+		g_Context->GlyphData.clear();
+	}
+
+	void Flush()
+	{
+		glClearColor(0.2f, 0.4f, 0.5f, 1.0f);
+		glViewport(0, 0, (GLsizei)g_Context->ViewportWidth, (GLsizei)g_Context->ViewportHeight);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		RenderRectangles();
+		RenderTexts();
+
+		glDisable(GL_BLEND);
+	}
 }
 
-void Resize(rendering_context* Context, f32 Width, f32 Height)
-{
-	Context->ViewportWidth  = Width;
-	Context->ViewportHeight = Height;
-}
-
-void SetTextScale(rendering_context* Context, f32 ScaleX, f32 ScaleY)
-{
-	Context->TextScaleX = ScaleX;
-	Context->TextScaleY = ScaleY;
-}
-
-void SetCameraInfo(rendering_context* Context, const f32* ViewMatrix, const f32* ProjMatrix)
-{
-	memcpy(Context->ViewMatrix, ViewMatrix, 16 * sizeof(f32));
-	memcpy(Context->ProjMatrix, ProjMatrix, 16 * sizeof(f32));
-}
-
-void DrawRectangle(rendering_context* Context,
-                   f32                X,
-                   f32                Y,
-                   f32                Width,
-                   f32                Height,
-                   color              Color,
-                   f32                Radius /* = 0.0f */,
-                   f32                BorderWidth /*= 1.0f*/,
-                   color              BorderColor /*= {0.0f, 0.0f, 0.0f, 1.0f}*/
+void DrawRectangle(f32   X,
+                   f32   Y,
+                   f32   Width,
+                   f32   Height,
+                   color Color,
+                   f32   Radius /* = 0.0f */,
+                   f32   BorderWidth /*= 1.0f*/,
+                   color BorderColor /*= {0.0f, 0.0f, 0.0f, 1.0f}*/
 )
 {
 	rectangle Rectangle;
@@ -231,15 +359,15 @@ void DrawRectangle(rendering_context* Context,
 	Rectangle.FillColorRadius   = Color;
 	Rectangle.FillColorRadius.A = Clamp(Radius, 0.0, Min(Width, Height) / 2.0f);
 
-	Context->Rectangles.push_back(Rectangle);
+	g_Context->Rectangles.push_back(Rectangle);
 }
 
-void SetFont(rendering_context* Context, const char* FontName)
+void SetFont(const char* FontName)
 {
-	Context->CurrentFont = FontName;
+	g_Context->CurrentFont = FontName;
 
 	// Load FontData if needed
-	if (Context->FontLookupMap.find(FontName) == Context->FontLookupMap.end())
+	if (g_Context->FontLookupMap.find(FontName) == g_Context->FontLookupMap.end())
 	{
 		auto Atlas = LoadFontAtlas(FontName);
 
@@ -248,22 +376,22 @@ void SetFont(rendering_context* Context, const char* FontName)
 		SetTextureWrapping(Texture, WrapMode_ClampToBorder, WrapMode_ClampToBorder);
 		SetTextureData(Texture, Atlas.Data);
 
-		Context->FontLookupMap[FontName] = (u32)Context->Fonts.size();
-		Context->FontTextures.push_back(Texture);
-		Context->Fonts.push_back(std::move(Atlas));
+		g_Context->FontLookupMap[FontName] = (u32)g_Context->Fonts.size();
+		g_Context->FontTextures.push_back(Texture);
+		g_Context->Fonts.push_back(std::move(Atlas));
 	}
 }
 
-void DrawText(rendering_context* Context, const char32_t* Text, f32 PixelSize, f32 X, f32 Y, color FillColor)
+void DrawText(const char32_t* Text, f32 PixelSize, f32 X, f32 Y, color FillColor)
 {
 	const char32_t* Char = Text;
 
 	f32 CursorX = X;
 	f32 CursorY = Y;
 
-	const u32 FontIndex = Context->FontLookupMap[Context->CurrentFont];
+	const u32 FontIndex = g_Context->FontLookupMap[g_Context->CurrentFont];
 
-	const auto& Atlas   = Context->Fonts[FontIndex];
+	const auto& Atlas   = g_Context->Fonts[FontIndex];
 	const auto& Metrics = Atlas.Metrics;
 
 	const f32 Scale = PixelSize / Metrics.LineHeight;
@@ -310,7 +438,7 @@ void DrawText(rendering_context* Context, const char32_t* Text, f32 PixelSize, f
 				WrittenGlyph.GlobalScale = Scale;
 				WrittenGlyph.TextureIndex = FontIndex;
 
-				Context->GlyphData.push_back(WrittenGlyph);
+				g_Context->GlyphData.push_back(WrittenGlyph);
 			}
 
 			CursorX += (Glyph.Advance * Scale);
@@ -318,133 +446,6 @@ void DrawText(rendering_context* Context, const char32_t* Text, f32 PixelSize, f
 
 		++Char;
 	}
-}
-
-void RenderRectangles(rendering_context* Context)
-{
-	const i32 RectangleCount = (i32)Context->Rectangles.size();
-
-	if (RectangleCount == 0)
-	{
-		return;
-	}
-
-	auto VertexTime   = fs::last_write_time(fs::path("shaders/rect.vert.glsl"));
-	auto FragmentTime = fs::last_write_time(fs::path("shaders/rect.frag.glsl"));
-
-	if (VertexTime > g_RectVShaderTime || FragmentTime > g_RectFShaderTime)
-	{
-		g_RectVShaderTime = VertexTime;
-		g_RectFShaderTime = FragmentTime;
-
-		auto VertexShaderHandle   = CreateShaderFromFile("shaders/rect.vert.glsl", ShaderType_Vertex);
-		auto FragmentShaderHandle = CreateShaderFromFile("shaders/rect.frag.glsl", ShaderType_Fragment);
-		auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
-
-		Context->RectProgram = ProgramHandle;
-	}
-
-	glUseProgram(Context->RectProgram.Idx);
-
-	const i32 ViewLoc         = glGetUniformLocation(Context->RectProgram.Idx, "u_View");
-	const i32 ProjLoc         = glGetUniformLocation(Context->RectProgram.Idx, "u_Proj");
-	const i32 ViewportSizeLoc = glGetUniformLocation(Context->RectProgram.Idx, "u_ViewportSize");
-
-	glUniformMatrix4fv(ViewLoc, 1, GL_FALSE, Context->ViewMatrix);
-	glUniformMatrix4fv(ProjLoc, 1, GL_FALSE, Context->ProjMatrix);
-	glUniform2f(ViewportSizeLoc, Context->ViewportWidth, Context->ViewportHeight);
-
-	if (Context->LastRectangleCount < RectangleCount)
-	{
-		UnmapBuffer(Context->RectangleInfoSSBO);
-		ResizeImmutableBuffer(&Context->RectangleInfoSSBO, RectangleCount * sizeof(rectangle));
-
-		Context->LastRectangleCount   = RectangleCount;
-		Context->RectangleInfoSSBOPtr = MapBuffer(Context->RectangleInfoSSBO);
-	}
-
-	memcpy(Context->RectangleInfoSSBOPtr, Context->Rectangles.data(), RectangleCount * sizeof(rectangle));
-
-	glBindVertexArray(Context->RectVertexArray.Idx);
-
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, Context->RectangleInfoSSBO.Idx);
-	glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr, RectangleCount);
-
-	Context->Rectangles.clear();
-}
-
-void RenderTexts(rendering_context* Context)
-{
-	const i32 GlyphCount = (i32)Context->GlyphData.size();
-
-	auto VertexTime   = fs::last_write_time(fs::path("shaders/text.vert.glsl"));
-	auto FragmentTime = fs::last_write_time(fs::path("shaders/text.frag.glsl"));
-
-	if (VertexTime > g_TextVShaderTime || FragmentTime > g_TextFShaderTime)
-	{
-		g_TextVShaderTime = VertexTime;
-		g_TextFShaderTime = FragmentTime;
-
-		auto VertexShaderHandle   = CreateShaderFromFile("shaders/text.vert.glsl", ShaderType_Vertex);
-		auto FragmentShaderHandle = CreateShaderFromFile("shaders/text.frag.glsl", ShaderType_Fragment);
-		auto ProgramHandle        = CreateProgram(VertexShaderHandle, FragmentShaderHandle, true);
-
-		Context->TextProgram = ProgramHandle;
-	}
-
-	glUseProgram(Context->TextProgram.Idx);
-
-	const i32 ViewLoc         = glGetUniformLocation(Context->TextProgram.Idx, "u_View");
-	const i32 ProjLoc         = glGetUniformLocation(Context->TextProgram.Idx, "u_Proj");
-	const i32 ViewportSizeLoc = glGetUniformLocation(Context->TextProgram.Idx, "u_ViewportSize");
-
-	glUniformMatrix4fv(ViewLoc, 1, GL_FALSE, Context->ViewMatrix);
-	glUniformMatrix4fv(ProjLoc, 1, GL_FALSE, Context->ProjMatrix);
-	glUniform2f(ViewportSizeLoc, Context->ViewportWidth, Context->ViewportHeight);
-
-	if (Context->LastGlyphCount < GlyphCount)
-	{
-		UnmapBuffer(Context->TextInfoSSBO);
-		ResizeImmutableBuffer(&Context->TextInfoSSBO, GlyphCount * sizeof(glyph_data));
-
-		Context->LastGlyphCount  = GlyphCount;
-		Context->TextInfoSSBOPtr = MapBuffer(Context->TextInfoSSBO);
-	}
-
-	memcpy(Context->TextInfoSSBOPtr, Context->GlyphData.data(), GlyphCount * sizeof(glyph_data));
-
-	glBindVertexArray(Context->RectVertexArray.Idx);
-
-	u32 CurrentIndex = 0;
-	for (const auto& Font : Context->FontTextures)
-	{
-		glBindTextureUnit(CurrentIndex++, Font.Idx);
-	}
-
-	i32 TextureBindings[] = {0, 1, 2, 3, 4, 5, 6, 7};
-
-	const i32 TexturesLoc = glGetUniformLocation(Context->TextProgram.Idx, "u_Textures");
-	glUniform1iv(TexturesLoc, 8, TextureBindings);
-
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, Context->TextInfoSSBO.Idx);
-	glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr, GlyphCount);
-
-	Context->GlyphData.clear();
-}
-
-void Flush(rendering_context* Context)
-{
-	glClearColor(0.2f, 0.4f, 0.5f, 1.0f);
-	glViewport(0, 0, (GLsizei)Context->ViewportWidth, (GLsizei)Context->ViewportHeight);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	RenderRectangles(Context);
-	RenderTexts(Context);
-
-	glDisable(GL_BLEND);
 }
 
 }
